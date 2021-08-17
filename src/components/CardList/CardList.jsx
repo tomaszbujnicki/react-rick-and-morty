65229ca266fc36616f22fe3dd00e231ccf17f35f
Tomasz Bujnicki <tomek.bujnicki@gmail.com>
2021-08-17 11:02:11 +0200

    Extend CardList

--- src/components/CardList/CardList.jsx
+++ src/components/CardList/CardList.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import CharacterCard from '../CharacterCard';
 import LocationCard from '../LocationCard';
 import EpisodeCard from '../EpisodeCard';
@@ -11,16 +11,29 @@ const CARDS = {
   episode: EpisodeCard,
 };
 
-const CardList = ({ items, type }) => {
+const CardList = ({ items, type, count = 20 }) => {
+  const [itemCount, setItemCount] = useState(count);
+
   if (items === null) return <Loading />;
   if (items === undefined) return <div></div>;
 
+  const showMore = () => setItemCount((prev) => prev + count);
+
+  const itemList = items.slice(0, itemCount);
+
   return (
-    <ul className="CardList">
-      {items.map((item, index) => (
-        <li key={index}>{CARDS[type]({ item })}</li>
-      ))}
-    </ul>
+    <>
+      <ul className="CardList">
+        {itemList.map((item, index) => (
+          <li key={index}>{CARDS[type]({ item })}</li>
+        ))}
+      </ul>
+      {items.length > itemCount && (
+        <button className="CardList__button" onClick={showMore}>
+          Show more ({items.length - itemCount} remaining)
+        </button>
+      )}
+    </>
   );
 };
 
