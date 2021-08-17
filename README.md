41607b9826ba6d901e6a74d361b7aa441b102a6e
Tomasz Bujnicki <tomek.bujnicki@gmail.com>
2021-08-17 14:37:25 +0200

    deploy

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # React Rick and Morty
+
+Live: <https://tomaszbujnicki.github.io/react-rick-and-morty/>
--- src/hooks/useSeo.js
+++ src/hooks/useSeo.js
@@ -4,14 +4,11 @@ const defaultDescription = 'The Rick and Morty React App.';
 const defaultTitle = 'Rick and Morty';
 
 const useSeo = ({ title, metaDescription = defaultDescription }) => {
-  console.log('in: ' + title);
   useEffect(() => {
-    console.log('effect: ' + title);
     document.title = title ? title + ' | ' + defaultTitle : defaultTitle;
   }, [title]);
 
   useEffect(() => {
-    console.log('effect: ' + metaDescription);
     document
       .querySelector('meta[name="description"]')
       .setAttribute('content', metaDescription);
